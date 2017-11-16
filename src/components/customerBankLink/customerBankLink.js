5ba69d7149a45575fb7562b5f1222514a7a42d0a
Shuang Liang <liangshuang@asiainfo.com>
2017-11-16 15:02:09 +0800

    校验

--- src/components/customerBankLink/customerBankLink.js
+++ src/components/customerBankLink/customerBankLink.js
@@ -4,29 +4,19 @@
 /* eslint-disable no-unused-vars,react/prefer-stateless-function */
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Table, Button, Modal, Form, Row, Col, Input, notification, message, Spin, Select, Icon } from 'antd'
+import { Table, Button, Modal, Form, Row, Col, Input, message, Select, Icon } from 'antd'
 import CustomerBankLinkWithForm from './customerBankLinkSearch'
 import EditBankLinkData from './editBankLinkData'
 import SelectCustomerWithForm from '../common/selectCustomer'
-import SelectCustomerWithFormForBankLink from '../common/selectCustomerForBankLink'
 
 
 const data = []
 const FormItem = Form.Item
 const Option = Select.Option
-// 显示提示
-const showNotificationWithIcon = (msg) => {
-  notification.error({
-    message: '错误',
-    description: msg,
-  })
-}
 class CustomerBankLink extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentPage: 1,
-      currentpageSize: 10,
       loading: false,
       delVisible: false,
       editVisible: false,
@@ -37,6 +27,7 @@ class CustomerBankLink extends React.Component {
       selectedRows: [],
       selectedRowKeys: [],
       tableHeight: '',
+      editModalData: [],
     }
   }
   componentWillMount() {
@@ -50,6 +41,7 @@ class CustomerBankLink extends React.Component {
       this.handleQuery()
     }
   }
+  // select change
   onSelectChange = (selectedRowKeys, selectedRows) => {
     this.setState({ selectedRowKeys, selectedRows })
   }
@@ -63,6 +55,10 @@ class CustomerBankLink extends React.Component {
     payBankAccount: '',
     status: '1',
   }
+  /*
+   function handleQuery
+   query data
+   */
   handleQuery = () => {
     this.setState({
       loading: true,
@@ -81,169 +77,167 @@ class CustomerBankLink extends React.Component {
       }
     })
   }
-  // 查询接口
+  /*
+   function handleChangeParam
+   params: param Query Condition
+   */
   handleChangeParam = (param) => {
     this.queryParam = { ...this.queryParam, ...param }
     this.handleQuery()
   }
+  /*
+   function handleChangeSize
+   params:
+    current: current pageNo
+    size: current pageSize
+   */
   handleChangeSize = (current, size) => {
     this.queryParam.pageInfo.pageNo = current
     this.queryParam.pageInfo.pageSize = size
     this.handleQuery()
   }
+  /*
+   function handleChangePage
+   params:
+   page: current page
+   */
   handleChangePage = (page) => {
     this.queryParam.pageInfo.pageNo = page
     this.handleQuery()
   }
-  // 显示人工添加modal
+  /*
+   function showAutoAdd
+    show add modal
+   */
   showAutoAdd = () => {
     this.setState({
       editVisible: true,
       edittitle: '人工添加客户银行关系数据',
+      editModalData: [],
     })
-    // 重新清空数据
-    this.props.form.resetFields()
   }
-  // 确认人工添加数据
-  handleAutoAddOk = () => {
-    this.setState({
-      editVisible: false,
-    })
-    const addData = this.props.form.getFieldsValue() // 获取modal数据
-    const custId = this.props.form.getFieldValue('erpCustId')
-    // 处理数据
-    if (custId) {
-      addData.erpCustId = custId[0]
-      addData.erpCustName = custId[1]
-    }
-    const postAddData = {}
-    postAddData.arcBankCust = addData
-    // console.log(postAddData)
-    // 提交数据
-    const testData = {
-      arcBankCust: {
-        custBankName: '招商银行',
-        custBankAccount: '62220282726',
-        erpCustName: '辽宁联通',
-        erpCustId: '2341',
-        sourceType: 'CBS',
-        receiptClaimId: '7641',
-      },
-    }
-    this.props.addArcCustBankData(postAddData).then((res) => {
-      if (res && res.response && res.response.resultCode === '000000') {
-        message.success('添加数据成功')
-      } else {
-        message.error('添加数据失败')
-      }
-    })
-    this.handleQuery()
-  }
-  // 显示编辑客户银行关系modal
+  /*
+   function showEditModal
+   params:
+    record : current data
+   show edit modal
+   */
   showEditModal = (record) => {
     this.setState({
       editVisible: true,
       edittitle: '编辑客户银行关系数据',
+      editModalData: record,
     })
-    record.erpCustId = [record.erpCustId, record.erpCustName]
-    // 赋值给modal
-    const { custBankName, erpCustId, custBankAccount, sourceType } = record
-    const x = { custBankName, erpCustId, custBankAccount, sourceType }
-    this.props.form.setFieldsValue(x)
   }
-  // 提交编辑客户银行关系数据 关闭modal
-  handleEditOk = (record) => {
+  /*
+    function submitAddData
+    params: addData post data
+    */
+  submitAddData = (addData) => {
     this.setState({
       editVisible: false,
+      editModalData: [],
     })
-    const newEditData = this.props.form.getFieldsValue()
-    const custId = this.props.form.getFieldValue('erpCustId')
-    // 处理数据
-    if (custId) {
-      newEditData.erpCustId = custId[0]
-      newEditData.erpCustName = custId[1]
-    }
-    const postEditData = {}
-    postEditData.arcBankCust = newEditData
-    // console.log('post', postEditData)
-    // 提交给后台接口
-    this.props.addArcCustBankData(postEditData).then((res) => {
+    this.props.addArcCustBankData(addData).then((res) => {
       if (res && res.response && res.response.resultCode === '000000') {
         message.success('添加数据成功')
       } else {
         message.error('添加数据失败')
       }
     })
-    // this.handleQuery()
   }
+  /*
+   function handleEditCancel
+   cancel edit modal data
+   */
   handleEditCancel = (e) => {
     this.setState({
       editVisible: false,
     })
   }
-  // 显示删除modal
-  showDelModal = (record) => {
-    // 返回数据给后台
-    // 删除成功显示按钮
-    this.setState({
-      delVisible: true,
-      selectedRows: record,
-    })
-  }
-  // 删除所选
-  delSelectData = () => {
-    if (this.state.selectedRows.length < 1) {
-      showNotificationWithIcon('必须选择至少一条数据')
-    } else {
-      // 提交数据
-      this.setState({
-        delVisible: true,
+  /*
+   function deleteDataFun
+   params:
+   delData : delete data
+   length: data length
+   type: 1-single delete  2-multi delete
+   delete data
+   */
+  deleteDataFun = (delData, length, type) => {
+    if (type === '2') {
+      this.props.deleteArcCustBankDatas(delData).then((res) => {
+        // console.log(res)
+        if (res && res.response && res.response.resultCode === '000000') {
+          message.success('成功失效' + length + '条数据')
+        } else {
+          message.error('删除数据失败')
+        }
       })
     }
-  }
-  // 删除单条
-  delOneData = () => {
-    const delDatas = this.state.selectedRows
-    // console.log(delDatas)
-    this.props.deleteArcCustBankData(delDatas.bankCustId).then((res) => {
-      // console.log(res)
-      if (res && res.response && res.response.resultCode === '000000') {
-        message.success('删除数据成功')
-      } else {
-        message.error('删除数据失败')
-      }
-    })
-    // this.handleQuery()
-  }
+    if (type === '1') {
+      this.props.deleteArcCustBankData(delData).then((res) => {
+        // console.log(res)
+        if (res && res.response && res.response.resultCode === '000000') {
+          message.success('删除数据成功')
+        } else {
+          message.error('删除数据失败')
+        }
+      })
+    }
+}
+  /*
+   function delOk
+   confirm delete data and hidden del modal
+   */
   delOk = () => {
     this.setState({
       delVisible: false,
     })
     const delLength = this.state.selectedRows.length
+    const delData = this.state.selectedRows
+    if (delLength === 0) {
+      message.error('请选择删除数据 ')
+      return
+    }
     if (delLength >= 1) {
-      // console.log(delLength)
       const delSelectData = {}
       const bankCustIds = []
-      this.state.selectedRows.forEach((item, index) => {
+      delData.forEach((item, index) => {
         bankCustIds[index] = item.bankCustId
       })
       delSelectData.bankCustIds = bankCustIds
-      // console.log('deletes', delSelectData)
-      this.props.deleteArcCustBankDatas(delSelectData).then((res) => {
-        // console.log(res)
-        if (res && res.response && res.response.resultCode === '000000') {
-          message.success('成功失效' + delLength + '条数据')
-        } else {
-          message.error('删除数据失败')
-        }
-      })
-      // this.handleQuery()
+      this.deleteDataFun(delSelectData, delLength, '2')
     } else {
-      this.delOneData()
+      this.deleteDataFun(delData.bankCustId, 1, '1')
     }
-
-    // 刷新数据
   }
+  /*
+ function showDelModal
+ show del modal
+ */
+  showDelModal = (record) => {
+    this.setState({
+      delVisible: true,
+      selectedRows: record,
+    })
+  }
+  /*
+   function showDelMultiModal
+   show multi del modal
+   */
+  showDelMultiModal = () => {
+    if (this.state.selectedRows.length < 1) {
+      message.error('必须选择至少一条数据')
+    } else {
+      this.setState({
+        delVisible: true,
+      })
+    }
+  }
+  /*
+   function delCancel
+   cancel delete data and hidden del modal
+   */
   delCancel = () => {
     this.setState({
       delVisible: false,
@@ -319,7 +313,7 @@ class CustomerBankLink extends React.Component {
       />
       <br />
       <Button onClick={this.showAutoAdd}>人工添加</Button>&nbsp;&nbsp;
-      <Button onClick={this.delSelectData}>删除</Button>
+      <Button onClick={this.showDelMultiModal}>删除</Button>
       <br /><br />
       <Table
         rowKey="bankCustId"
@@ -333,88 +327,13 @@ class CustomerBankLink extends React.Component {
         scroll={{ x: '120%', y: this.state.tableHeight }}
       />
       { /* 编辑客户银行关系modal */ }
-      {/*  */}
-      <Modal
+      {this.state.editVisible ? <EditBankLinkData
         title={this.state.edittitle}
         visible={this.state.editVisible}
         onCancel={this.handleEditCancel}
-        onOk={this.state.edittitle === '编辑客户银行关系数据' ? this.handleEditOk : this.handleAutoAddOk}
-      >
-        <Form
-          className="ant-search-form"
-        >
-          <Row gutter={40}>
-            <Col span={12} key={1}>
-              <FormItem {...formItemLayout} label="客户名称">
-                {getFieldDecorator('erpCustId', {
-                  initialValue: [],
-                })(<SelectCustomerWithForm />)}
-              </FormItem>
-            </Col>
-            <Col span={12} key={2}>
-              <FormItem {...formItemLayout} label="银行名称">
-                {getFieldDecorator('custBankName', {
-                  initialValue: '',
-                })(
-                  <Input
-                    placeholder="请输入银行名称"
-                  />,
-                )}
-              </FormItem>
-            </Col>
-          </Row>
-          <Row gutter={40}>
-            <Col span={12} key={3}>
-              <FormItem {...formItemLayout} label="银行帐号">
-                {getFieldDecorator('custBankAccount', {
-                  rules: [{ pattern: /^[+]{0,1}(\d+)$/, message: '请输入正确银行帐号' }],
-                  initialValue: '',
-                })(
-                  <Input
-                    placeholder="请输入银行帐号"
-                  />,
-                )}
-              </FormItem>
-            </Col>
-            <Col span={12} key={5}>
-              <FormItem {...formItemLayout} label="关系来源">
-                {getFieldDecorator('sourceType', {
-                  initialValue: '',
-                })(
-                  <Input
-                    placeholder="请输入关系来源"
-                  />,
-                )}
-              </FormItem>
-            </Col>
-          </Row>
-          {/* <Row gutter={40}>
-            <Col span={12} key={3}>
-              <FormItem {...formItemLayout} label="数据状态">
-                {getFieldDecorator('status', {
-                  initialValue: '1',
-                })(
-                  <Select
-                    placeholder="请选择数据状态"
-                    notFoundContent=""
-                    defaultActiveFirstOption={false}
-                    filterOption={false}
-                    onChange={this.handleChange}
-                  >
-                    <Option value="1">有效</Option>
-                    <Option value="0">无效</Option>
-                  </Select>,
-                )}
-              </FormItem>
-            </Col>
-          </Row> */}
-          {getFieldDecorator('bankCustId')(
-            <Input
-              type="hidden"
-            />,
-          )}
-        </Form>
-      </Modal>
+        queryParams={this.submitAddData}
+        editModalData={this.state.editModalData}
+      /> : null}
       { /* 删除数据modal */ }
       <Modal
         visible={this.state.delVisible}
@@ -438,6 +357,7 @@ CustomerBankLink.propTypes = {
     getFieldValue: PropTypes.func.isRequired,
     setFieldsValue: PropTypes.func.isRequired,
     resetFields: PropTypes.func.isRequired,
+    validateFields: PropTypes.func.isRequired,
   }).isRequired,
   arcCustBankLink: PropTypes.shape({
     getBankLinkList: PropTypes.arrayOf.isRequired,
