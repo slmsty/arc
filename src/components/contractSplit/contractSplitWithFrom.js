73d6a373e61cfdf5f782ec7e736bc4075f8cedc1
Shuang Liang <liangshuang@asiainfo.com>
2018-03-23 10:32:25 +0800

    合同拆分

--- src/components/contractSplit/contractSplitWithFrom.js
+++ src/components/contractSplit/contractSplitWithFrom.js
@@ -144,9 +144,9 @@ class ContractSplitCon extends React.Component {
             </Col>
           </Row>
           <Row gutter={40}>
-            <Col span={8}>
+            {/*<Col span={8}>
               <FormItem {...formItemLayout} label="是否复算项目">
-                {getFieldDecorator('复选', {
+                {getFieldDecorator('recalculate', {
                   initialValue: 'ALL',
                 })(
                   <Select
@@ -159,8 +159,8 @@ class ContractSplitCon extends React.Component {
                   </Select>,
                 )}
               </FormItem>
-            </Col>
-            <Col span={16} style={{ textAlign: 'right' }}>
+            </Col>*/}
+            <Col span={24} style={{ textAlign: 'right' }}>
               <Button type="primary" key="search" onClick={this.handleQuery}><Icon type="search" />查询</Button>
             </Col>
           </Row>
