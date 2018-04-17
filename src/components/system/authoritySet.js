4a8917b9a09026d7a424f8d95692293f69e164aa
pengxw <pengxw@asiainfo.com>
2018-04-17 11:52:16 +0800

    权限系统添加组件

---
+++ src/components/system/authoritySet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AuthoritySet extends React.Component {
+  render() {
+    return (
+      <div>
+        权限设置，请到新打开的权限系统配置权限
+      </div>
+    )
+  }
+}
+
+export default AuthoritySet
