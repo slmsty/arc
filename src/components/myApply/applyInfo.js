2f39e5b3ceaa6a0bc6ed4753fab9e6ac43d8c864
pengxw <pengxw@asiainfo.com>
2018-06-27 15:38:30 +0800

    去除多提交的代码

--- src/components/myApply/applyInfo.js
+++ src/components/myApply/applyInfo.js
@@ -7,7 +7,7 @@ import { Modal, Row, Col, Button, Input, Form, Table, message } from 'antd'
 import BillApproveDetail from './billApproveDetail'
 import UrlModalCom from '../common/getUrlModal'
 import BillDetail from './billDetail'
-import ReceiptApplyDetail from './receiptApplyDetail'
+//import ReceiptApplyDetail from './receiptApplyDetail'
 import requestJsonFetch from '../../http/requestJsonFecth'
 import './billApproveDetail.less'
 
@@ -314,13 +314,13 @@ class ApplyInfoModal extends React.Component {
                 </div>
                 : null
             }
-            {
+            {/*{
               applyInfoDatas.serviceType === 'RECEIPT' ?
                 <div>
                   <h2>收据申请</h2>
                   <ReceiptApplyDetail />
                 </div> : null
-            }
+            }*/}
             <br />
             <br />
             <hr style={{ borderTop: '1px solid #d9d9d9' }} />
