06bb64466b441043a2f20fbd024a9ddfb1f4eb1f
pengxw <pengxw@asiainfo.com>
2018-07-11 17:25:58 +0800

    注释收件人信息

--- src/components/myApply/billApproveDetail.js
+++ src/components/myApply/billApproveDetail.js
@@ -1241,7 +1241,7 @@ class BillApproveDetail extends React.Component  {
                 </FormItem>
               </Col>
             </Row>
-            <h3 className="sent-info">寄件信息</h3>
+            {/*<h3 className="sent-info">寄件信息</h3>
             <Row gutter={40}>
               <Col span={8} key={1}>
                 <FormItem {...formItemLayout2} label="收件人">
@@ -1308,7 +1308,7 @@ class BillApproveDetail extends React.Component  {
                   )}
                 </FormItem>
               </Col>
-            </Row>
+            </Row>*/}
           </div>
         }
         {
